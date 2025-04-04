dc675e9cea6d407ab1b95f40512fda2f6987aa4b
hyuyu1012 <hanhyeongu495@gmail.com>
2025-04-04 14:14:41 +0900

    4/4

--- faq-accordion-main/script.js
+++ faq-accordion-main/script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.faq_box').forEach(faq => {
+  document.querySelectorAll('.faq_box').forEach(faq => {
   const plusBtn = faq.querySelector('.plus');
   const minusBtn = faq.querySelector('.minus');
   const answer = faq.querySelector('.answer'); 
